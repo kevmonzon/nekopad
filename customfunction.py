c8df5e3fb7625caee5358df3b3c6e3bce72c796a
Rae Kevin Turno Monzon <kevmonzon@gmail.com>
2024-05-04 17:47:34 +0800

    example codes

--- customfunction.py
+++ customfunction.py
@@ -1,10 +1,11 @@
 class CustomFunction:
-    def __init__(self, function, *kwargs):
+    def __init__(self, function, *args, **kwargs):
         self.function = function
+        self.args = args
         self.kwargs = kwargs
 
     def press(self, macropad):
-        self.function(macropad, *self.kwargs)
+        self.function(macropad, *self.args, **self.kwargs)
 
     def release(self, macropad):
         macropad.keyboard.release_all()
